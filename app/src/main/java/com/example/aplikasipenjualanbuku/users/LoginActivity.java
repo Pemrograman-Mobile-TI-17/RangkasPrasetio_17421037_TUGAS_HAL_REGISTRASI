12a88d6d6880bc08624ea2045e4e028399d07cae
RangkasPrasetio <brownjack6996@gmail.com>
2020-04-16 11:22:37 +0700

    TUGAS REGISTRASI

---
+++ app/src/main/java/com/example/aplikasipenjualanbuku/users/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.aplikasipenjualanbuku.users;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aplikasipenjualanbuku.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void register(View view) {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        }
+    }
+
---
+++ app/src/main/java/com/example/aplikasipenjualanbuku/users/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.example.aplikasipenjualanbuku.users;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aplikasipenjualanbuku.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
